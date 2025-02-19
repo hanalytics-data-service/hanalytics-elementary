0bb158d13836cd11186e000af57f1e367dd091d1
Ofek Weiss <ofek1weiss@gmail.com>
2025-02-19 17:49:36 +0200

    Added formatting needed for critical assets

--- elementary/messages/formats/adaptive_cards.py
+++ elementary/messages/formats/adaptive_cards.py
@@ -17,6 +17,7 @@ from elementary.messages.blocks import (
     TableBlock,
     TextBlock,
     TextStyle,
+    WhitespaceBlock,
 )
 from elementary.messages.formats.html import ICON_TO_HTML
 from elementary.messages.message_body import Color, MessageBlock, MessageBody
@@ -54,6 +55,8 @@ def format_inline_block(block: InlineBlock) -> str:
         return block.user
     elif isinstance(block, LineBlock):
         return format_line_block_text(block)
+    elif isinstance(block, WhitespaceBlock):
+        return "&emsp;"
     else:
         raise ValueError(f"Unsupported inline block type: {type(block)}")
 
